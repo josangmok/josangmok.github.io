b9032f82c6b8fd70932dafb42d408c59ae94ea27
josangmok <105770899+josangmok@users.noreply.github.com>
2022-05-25 18:47:40 +0900

    Add files via upload

--- trn01/js/main.js
+++ trn01/js/main.js
@@ -5,7 +5,11 @@ $(function () {
     autoplay: true,
     pauseOnHover: false,
     pauseOnFocus: false,
+    fade: true,
   })
 })
 
-
+$('.main_slider').on('init reinitafterChange', function()
+{ var here = $('.slick-current');
+  here.addClass('on').siblings().removeClass('xxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
+})
---
+++ 새 폴더/position.js
@@ -0,0 +1,7 @@
+$(function(){
+  $('.smile li').on('click' , function(){
+    var idx = $(this).index();
+    console.log(idx)
+    $('.smile .case ul').css({}) 
+})
+})
